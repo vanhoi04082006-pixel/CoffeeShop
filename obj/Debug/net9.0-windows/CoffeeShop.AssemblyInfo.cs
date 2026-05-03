7935496dc2f0208c8a1b361156c052655f994ac3
CodeTech <buivanhoi10a2@gmail.com>
2026-05-03 14:25:18 +0700

    fix

--- obj/Debug/net9.0-windows/CoffeeShop.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/CoffeeShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df5132f15628f06260895ee0a3d1b5ffd1ba2d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39800021584726c96ce4b6c4814e26144ced862e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
